30b4164dc96cfb561d97d42b6a3aec9865ca661f
Nesko <nseko@techaffinity.com>
2022-07-08 17:35:08 +0200

    package ovrtt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -96,6 +96,7 @@ export class AuthService {
   }
 
   async userLogin(dto: userSignInDto): Promise<{}> {
+    let verified = false;
     const user = await this.prisma.user.findFirst({
       where: { email: dto.email, AND: [{ isActive: true }] },
     });
